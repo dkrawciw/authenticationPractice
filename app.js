7e2605fee370933cdcf7160c6e840118b5ab5862
Daniel Krawciw <daniel.krawciw@gmail.com>
2022-07-24 18:16:06 -0600

    Added ejs partials

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express'),
       app = express(),
       customBasicAuth = require('./routes/customBasicAuth.js');
 
+app.set('view engine', 'ejs');
+app.use( express.static('public') );
+
+// Landing Page
 app.get('/', (req, res) => {
-    res.send("Howdy!")
+    res.render('index.ejs')
 })
 
+// Including Routes
 app.use('/customBasicAuth', customBasicAuth)
 
+// Define the port to listen on
 app.listen(8080, () => {
     console.log("Running Authentication App...")
 })
