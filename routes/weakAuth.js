2f74ae0368e14db8d25886c7984c31b97e884f53
Daniel Krawciw <daniel.krawciw@gmail.com>
2022-07-26 23:25:03 -0600

    fixed scaling issue with text overflowing when screen width is small

--- routes/weakAuth.js
+++ routes/weakAuth.js
@@ -2,19 +2,19 @@ const express = require('express'),
       router = express.Router();
 
 // Setting up routes for logging in
-router.get('weakAuth/login', (req, res) => {
+router.get('/weakAuth/login', (req, res) => {
     res.render('weakAuthLogin.ejs')
 })
-router.post('weakAuth/login', (req, res) => {
+router.post('/weakAuth/login', (req, res) => {
     console.log('Logged in, Hype!')
     res.redirect('/')
 })
 
 // Setting up routes for signing up
-router.get('weakAuth/signup', (req, res) => {
+router.get('/weakAuth/signup', (req, res) => {
     res.render('weakAuthSignup.ejs')
 })
-router.post('weakAuth/signup', (req, res) => {
+router.post('/weakAuth/signup', (req, res) => {
     console.log('Signed Up, Hype!')
     res.redirect('/')
 })
