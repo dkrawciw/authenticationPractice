ed0be4ee98a06c9fdccb9a79af802eb84900f637
Daniel Krawciw <daniel.krawciw@gmail.com>
2022-07-30 13:32:34 -0600

    Added http and https with self-generated certificates

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express'),
       app = express();
 
+// Pieces necessary to run the server on https
+const fs = require('fs'),
+      http = require('http'),
+      https = require('https');
+
 // Connected JS pages
 const passAuth = require('./routes/passAuth.js'),
       smsAuth = require('./routes/smsAuth.js');
 
-// Placeholder
+// Port values
+const HTTP_PORT = 8080,
+      HTTPS_PORT = 8181;
+
+
+
+// Placeholder options
+// NOTE: Should probably find a better way to get and send this information
 let options = [
     {
         name: 'Password Authentication',
@@ -43,6 +55,30 @@ app.get('/*', (req, res) => {
 })
 
 // Define the port to listen on
+/*
 app.listen(8080, () => {
-    console.log("Running Authentication App...")
+    console.log('Running Authentication App...')
+})
+*/
+
+// running the server with http
+http.createServer(app)
+.listen(HTTP_PORT, () => {
+    console.log('HTTP app running on port: ' + HTTP_PORT)
+})
+
+// Running the server with https
+/* 
+Certificates generated with command:
+    openssl req -nodes -new -x509 -keyout server.key -out server.cert
+*/
+
+https
+.createServer(
+    {
+        key: fs.readFileSync('server.key'),
+        cert: fs.readFileSync('server.cert')
+    }, app)
+.listen(HTTPS_PORT, () => {
+    console.log('HTTPS app running on port: ' + HTTPS_PORT)
 })
